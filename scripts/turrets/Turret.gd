e1fced4e07433e84e79824373c3b818a2a4a6191
Chris <boyd111c@gmail.com>
2026-04-20 14:11:27 -0600

    better targetting

--- scripts/turrets/Turret.gd
+++ scripts/turrets/Turret.gd
@@ -3,3 +3,9 @@ extends Node2D
 
 @onready var barrel: Node2D = %Barrel
 
+
+## Uses DestructibleTarget row-leftmost cache; `from_local` in target space (e.g. `dt.to_local(global_position)`).
+func pick_closest_row_leftmost_cell(dt: DestructibleTarget, from_local: Vector2) -> Vector2i:
+	if dt == null or dt.is_destroyed():
+		return Vector2i(-1, -1)
+	return dt.get_closest_row_leftmost_cell(from_local)
